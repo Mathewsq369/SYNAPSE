c772ebe1f35f1163f14fce0387b9622942c49acd
WacoderForever <sethomondi088@gmail.com>
2025-03-22 00:58:48 +0300

    init_views

--- SYNAPSE/store/views.py
+++ SYNAPSE/store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+#Create views here.
+def store(request):
+     context = {}
+     return render(request, 'store/store.html', context)
 
-# Create your views here.
+def cart(request):
+     context = {}
+     return render(request, 'store/cart.html', context)
+
+def checkout(request):
+      context = {}
+      return render(request, 'store/checkout.html', context)
